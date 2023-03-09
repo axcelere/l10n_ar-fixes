2aef3393c952edaaed276c493b7c7b466d5c6fd5
ernesto <emato@axcelere.com>
2023-03-09 10:29:16 -0500

    [ADD] Adding new fix padron iibb

--- l10n_ar_account_withholding_fix_padron_iibb/models/res_company_jurisdiction_padron.py
+++ l10n_ar_account_withholding_fix_padron_iibb/models/res_company_jurisdiction_padron.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 from io import BytesIO
+from datetime import datetime
 import zipfile
 import tempfile
 import os
@@ -14,6 +15,11 @@ class ResCompanyJurisdictionPadron(models.Model):
     _inherit = "res.company.jurisdiction.padron"
     _description = "res.company.jurisdiction.padron"
 
+    # file_padron = fields.Binary(
+    #     "File",
+    #     required=True,
+    # )
+
     @api.constrains('jurisdiction_id')
     def check_jurisdiction_id(self):
         pass
@@ -21,3 +27,25 @@ class ResCompanyJurisdictionPadron(models.Model):
         # for rec in self:
         #     if rec.jurisdiction_id != arba_tag:
         #         raise ValidationError("El padron para (%s) no está implementado." % rec.jurisdiction_id.name)
+
+    def generate_alicuota(self):
+        stream = BytesIO(base64.b64decode(self.file_padron)).read()
+        total_len = len(str(stream).split("\\r\\n")) - 1
+        cant = 0
+        for line in str(stream).split("\\r\\n"):
+            cant+= 1
+            if cant <= total_len:
+                values = line.split(";")
+                partner_id = self.env['res.partner'].search([('vat', '=', values[4])])
+                if partner_id:
+                    vals = {
+                        'numero_comprobante': '00',
+                        # 'alicuota_retencion': float(alicuot_ret),
+                        'alicuota_percepcion': values[8].replace(",", "."),
+                        'partner_id': partner_id.id,
+                        'company_id': self.company_id.id,
+                        'tag_id': self.jurisdiction_id.id,
+                        'from_date': self.l10n_ar_padron_from_date,
+                        'to_date': self.l10n_ar_padron_to_date,
+                    }
+                    self.env['res.partner.arba_alicuot'].create(vals)
