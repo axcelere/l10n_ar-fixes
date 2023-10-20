721de35f1d2627718c71822cfc552625e5630ff4
rafaelaxcelere <rafael@axcelere.com>
2023-10-20 17:20:01 -0300

    [ADD][l10n_ar_account_withholding_fix_padron_iibb] Seguridad

--- l10n_ar_account_withholding_fix_padron_iibb/models/res_company_jurisdiction_padron.py
+++ l10n_ar_account_withholding_fix_padron_iibb/models/res_company_jurisdiction_padron.py
@@ -1,5 +1,6 @@
 import base64
 import logging
+import os
 from io import BytesIO
 
 from odoo import models, api, fields
@@ -21,6 +22,9 @@ class ResCompanyJurisdictionPadron(models.Model):
         #     if rec.jurisdiction_id != arba_tag:
         #         raise ValidationError("El padron para (%s) no está implementado." % rec.jurisdiction_id.name)
 
+    def _condition_toclean_line(self, line, partners_vat):
+        return ";0,00;0,00;00;00;" in line
+
     def generate_alicuota_fromzip(self):
         # 26092023;01102023;31102023;20000163989;D;S;N;0,00;0,00;00;00;ETCHEVERRIGARAY JUAN  CARLOS
         # fecha;fecha_inicio;fecha_fin;vat;D;S;N;alicuota_percepcion;alicuota_retencion
@@ -29,28 +33,43 @@ class ResCompanyJurisdictionPadron(models.Model):
         self.descompress_file(self.file_padron)
         filename = self.pdf_filename.replace('.zip', '')
         path_file = "/tmp/%s/%s.txt" % (filename, filename)
+        temp_path_file = "/tmp/%s/%s_temp.txt" % (filename, filename)
+
+        partners = self.env['res.partner'].search([])
+        partners_vat = partners.mapped('vat')
+        partner_dict = {partner.vat: partner.id for partner in partners}
+
+        with open(path_file, "r") as input_f:
+            lines_to_keep = [line for line in input_f if not self._condition_toclean_line(line, partners_vat)]
+
+        with open(temp_path_file, "w") as output_f:
+            output_f.writelines(lines_to_keep)
+
+        os.replace(temp_path_file, path_file)
+
         with open(path_file, "r", errors="replace") as fp:
             line_number = 1
+            bulk_vals = []
             for line_bytes in fp:
                 line = line_bytes.strip()
                 _logger.log(25, line)
                 _logger.log(25, "************LINEA %s************" % str(line_number))
                 line_number += 1
                 values = line.split(";")
-                partner_id = Partner.search([('vat', '=', values[3])], limit=1)
+                partner_id = partner_dict.get(values[3], False)
                 if partner_id:
-                    vals = {
+                    bulk_vals.append({
                         'numero_comprobante': '',
                         'alicuota_percepcion': values[7].replace(",", "."),
                         'alicuota_retencion': values[8].replace(",", "."),
-                        'partner_id': partner_id.id,
+                        'partner_id': partner_id,
                         'company_id': self.company_id.id,
                         'tag_id': self.jurisdiction_id.id,
                         'from_date': self.l10n_ar_padron_from_date,
                         'to_date': self.l10n_ar_padron_to_date,
-                    }
-                    _logger.log(25, "Nueva alicuota a partner: %s" % partner_id.display_name)
-                    Alicuot.sudo().create(vals)
+                    })
+                    _logger.log(25, "Nueva alicuota a partner: %s" % partner_id)
+            Alicuot.sudo().create(bulk_vals)
 
     def generate_alicuota(self):
         stream = BytesIO(base64.b64decode(self.file_padron)).read()
